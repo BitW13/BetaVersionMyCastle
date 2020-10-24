a7c18333b9853e2bc5034c8c0aa1dc8a0439b17c
Artsiom_Shabolda <artsiom_shabolda@epam.com>
2020-10-24 11:32:36 +0300

    master - added unuseful enter

--- TaskPlannerService/TaskPlannerService.WebApi/Startup.cs
+++ TaskPlannerService/TaskPlannerService.WebApi/Startup.cs
@@ -27,6 +27,7 @@ namespace TaskPlannerService.WebApi
             services.AddCors();
             services.AddAutoMapper();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
